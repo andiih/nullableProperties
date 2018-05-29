0a0f2989164df0321ec136a244349a0a57a2e661
Andy Hawken (SP4) <andrew.hawken@fiftyeggs.co.uk>
2018-05-29 22:53:41 +0100

    Original junk from Sum project

---
+++ source/original/NullableIntPropertyConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+
+namespace Sum.Umbraco.App_Plugins.SumNullablePropEditors
+{
+    public class NullableIntPropertyConverter : IPropertyValueConverterMeta
+    {
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias == "sum.NullableInt";
+        }
+
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var attempt = source.TryConvertTo<int?>();
+            if (attempt.Success)
+            {
+                return attempt.Result;
+            }
+
+            return null;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+
+            if (source == null || UmbracoContext.Current == null)
+            {
+                return null;
+            }
+            return ConvertDataToSource(propertyType,source,preview);
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source.ToString();
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(int?);
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            PropertyCacheLevel returnLevel;
+            switch (cacheValue)
+            {
+                case PropertyCacheValue.Object:
+                    returnLevel = PropertyCacheLevel.ContentCache;
+                    break;
+                case PropertyCacheValue.Source:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                case PropertyCacheValue.XPath:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                default:
+                    returnLevel = PropertyCacheLevel.None;
+                    break;
+            }
+
+            return returnLevel;
+        }
+    }
+}
---
+++ source/original/NullableNumberPropertyConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+
+namespace Sum.Umbraco.App_Plugins.SumNullablePropEditors
+{
+    public class NullableNumberPropertyConverter : IPropertyValueConverterMeta
+    {
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias == "sum.NullableNum";
+        }
+
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var attempt = source.TryConvertTo<decimal?>();
+            if (attempt.Success)
+            {
+                return attempt.Result;
+            }
+
+            return null;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+
+            if (source == null || UmbracoContext.Current == null)
+            {
+                return null;
+            }
+            return ConvertDataToSource(propertyType, source, preview);
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source.ToString();
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(decimal?);
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            PropertyCacheLevel returnLevel;
+            switch (cacheValue)
+            {
+                case PropertyCacheValue.Object:
+                    returnLevel = PropertyCacheLevel.ContentCache;
+                    break;
+                case PropertyCacheValue.Source:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                case PropertyCacheValue.XPath:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                default:
+                    returnLevel = PropertyCacheLevel.None;
+                    break;
+            }
+
+            return returnLevel;
+        }
+
+    }
+}
